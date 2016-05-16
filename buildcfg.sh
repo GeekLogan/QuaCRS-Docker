c03b983a9a6310706a669db19f3e9a29b8da37eb
Logan Walker <walker.1698@osu.edu>
2016-05-16 09:12:03 -0400

    Add buildcfg script

---
+++ buildcfg.sh
@@ -0,0 +1,15 @@
+echo "WGF=$(find /data -name *.fa)" > /QuaCRS/QC_Pack/tools.cfg
+echo "ANNOT=$(find /data -name *.gtf)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "ANNOT_BED=$(find /data -name *.bed)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "FASTQC_EXEC=$(find / | fgrep fastqc | fgrep -v .)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "PICARD_JAR=$(find / -name 'picard.jar')" >> /QuaCRS/QC_Pack/tools.cfg
+echo "RNASEQC_JAR=$(find / | fgrep RNASeQC | fgrep jar)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "SAMTOOLS_EXEC=$(which samtools)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "RSEQC_DIR=$(find / -name "read_quality.py" | sed 's/read_quality.py//')" >> /QuaCRS/QC_Pack/tools.cfg
+echo "FEATURECOUNTS_EXEC=$(find / | fgrep featureCounts | fgrep -v man)" >> /QuaCRS/QC_Pack/tools.cfg
+echo "lncRNA_genes=" >> /QuaCRS/QC_Pack/tools.cfg
+echo "lincRNA_genes=" >> /QuaCRS/QC_Pack/tools.cfg
+echo "coding_genes=" >> /QuaCRS/QC_Pack/tools.cfg
+echo "housekeeping_genes=" >> /QuaCRS/QC_Pack/tools.cfg
+echo "other_genes=" >> /QuaCRS/QC_Pack/tools.cfg
+echo "threads=6" >> /QuaCRS/QC_Pack/tools.cfg
